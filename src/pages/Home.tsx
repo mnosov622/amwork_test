ea17d992b6fda1aa91fed4c7d6685704e03c6902
Maxim N <mnosov622@gmail.com>
2023-09-01 04:57:50 -0400

    tasks headline init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import TaskCard from "../components/TaskCard";
+import addTaskIcon from "../assets/add_bold.svg";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <>
+      <section className="flex justify-space-between align-center">
+        <h1 className="green-bold">Today</h1>
+        <section className="flex">
+          <img className="cursor_pointer" src={addTaskIcon} alt="add task" />
+          <div className="tasks_count">2</div>
+        </section>
+      </section>
+      <TaskCard />
+    </>
+  );
+};
+
+export default Home;
